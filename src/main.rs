95b2f352438912122a3c61d9a74fd699bad1a9fa
Bartosz Moczulski <moczulski@gmail.com>
2024-12-21 19:36:41 +0100

    Initial web server implementation (no real data yet)
    This adds initial server boilerplate. It will bind to 0.0.0.0:9192, which
    can be overridden via environment variables:
    - OWNTRACKS_EXPORTER_BIND_HOST
    - OWNTRACKS_EXPORTER_BIND_PORT
    
    It will export one example metric (just for testing) which will later be
    replaced with actual set of metrics computed against real OwnTracks
    Recoder storage directory.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,60 @@
+// Will create an exporter with a single metric that does not change
+
+use env_logger::{
+    Builder,
+    Env,
+};
+use log::{debug, info, trace};
+use prometheus_exporter::prometheus::register_gauge;
+use std::net::SocketAddr;
+
+fn get_addr() -> SocketAddr {
+    let host = std::env::var("OWNTRACKS_EXPORTER_BIND_HOST")
+        .unwrap_or("0.0.0.0".to_string());
+    let port = std::env::var("OWNTRACKS_EXPORTER_BIND_PORT")
+        .unwrap_or("9192".to_string());
+    let addr_str = format!("{host}:{port}");
+    addr_str
+        .parse()
+        .expect(format!("This doesn't seem to be a valid bind address: {addr_str}").as_str())
+}
+
 fn main() {
-    println!("Hello, world!");
+    // Setup logger with default level info so we can see the messages from
+    // prometheus_exporter.
+    Builder::from_env(Env::default().default_filter_or("info")).init();
+
+    // Parse address used to bind exporter to.
+    let addr = get_addr();
+
+    // First self-request to make sure the server started correctly
+    let barrier = std::sync::Arc::new(std::sync::Barrier::new(2));
+    {
+      let barrier = barrier.clone();
+      std::thread::spawn(move || {
+        trace!("waiting on client barrier");
+        barrier.wait();
+        let body = reqwest::blocking::get(format!("http://{addr}")).unwrap().text().unwrap();
+        info!("initial metrics look fine:\n{body}");
+      });
+    }
+
+    // Create metric
+    let metric = register_gauge!("simple_the_answer", "to everything")
+        .expect("can not create gauge simple_the_answer");
+
+    // Start exporter
+    let exporter = prometheus_exporter::start(addr).expect("can not start exporter");
+
+    barrier.wait();
+    metric.set(42.0);
+
+    loop {
+        debug!("waiting for next request...");
+        let guard = exporter.wait_request();
+        debug!("updating metrics...");
+        metric.inc();
+        debug!("updating metrics...");
+        drop(guard);
+    }
 }
